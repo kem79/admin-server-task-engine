4b0eadd1c92428110ed8457238c18ef30f3fd781
Marc Marechal <marc.marechal@emc.com>
2019-04-22 17:01:35 +0800

    update data in data-emetrics locust file

--- my_locust/locustfile_data_metrics_api.py
+++ my_locust/locustfile_data_metrics_api.py
@@ -1,17 +1,18 @@
-from locust import HttpLocust, TaskSet, seq_task
 
+
+from locust import HttpLocust, TaskSet, task, seq_task
 
 class DataMetricsService(TaskSet):
 
     head = {"x-app-key": "804c41d8cfd74926185087402f166277",
             "uid": "1206540"}
-    vcenterId = '168866bf-8390-4d24-b5b2-b57459de6d1a'
-    datacenter = 'L4VXR2_DC'
-    clusterId = '52f38501-cbc3-d779-683b-c7964870c48e'
-    hostId = '4c4c4544-004a-3510-8036-c4c04f485132'
+    vcenterId = '27cf6667-26df-4c72-9d32-424bbca30b09'
+    datacenter = 'VxRail-Datacenter'
+    clusterId = '522dfb0c-20cc-d2f9-9849-0c18707289b4'
+    hostId = '8c7c8df8-7c53-1000-8d6b-54ab3a70aa76'
     dcParam = '?data_center=%s&vcenter_uuid=%s' % (datacenter, vcenterId)
     clusterParam = '?cluster_uuid=%s' % clusterId
-    clusterParam4usage = '?cluster_uuid=5242fd84-1c73-2279-38aa-f5d2020e6f64'
+    clusterParam4usage = '?cluster_uuid=522dfb0c-20cc-d2f9-9849-0c18707289b4'
 
     # One task per page that calls data-metrics-service
 
@@ -41,16 +42,16 @@ class DataMetricsService(TaskSet):
     def hosts_performance_summary(self, params):
         metricType = 'cpu%2Cmem%2Cdisk%2Cnet'
         # Time range is 24 hours as UI default
-        fromTime = '1554862256241'
-        toTime = '1554948656241'
+        fromTime = '1555904860148'
+        toTime = '1555912060149'
         self.sendGetRequest('/api/v1/hosts/performance/summary%s&metric_type=%s&from_timestamp=%s&to_timestamp=%s'
                             % (params, metricType, fromTime, toTime))
 
     def hosts_performance_details(self, params):
         metricType = 'cpu%2Cmem%2Cdisk%2Cnet'
         # Time range is 24 hours as UI default
-        fromTime = '1554862256241'
-        toTime = '1554948656241'
+        fromTime = '1555904860148'
+        toTime = '1555912060149'
         self.sendGetRequest('/api/v1/hosts/performance/details%s&host_uuid=%s&from_timestamp=%s&to_timestamp=%s&metric_type=%s&%s'
                             % (params, self.hostId, fromTime, toTime, metricType, 'show_anomalies=true'))
 
