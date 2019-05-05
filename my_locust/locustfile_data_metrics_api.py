f897e1a90961b39c88d21551d36334e7a030fcd9
e <Yolanda.Li1@vce.com>
2019-05-05 11:31:53 +0800

    split performance detail request using data metric type

--- my_locust/locustfile_data_metrics_api.py
+++ my_locust/locustfile_data_metrics_api.py
@@ -1,11 +1,9 @@
-
-
 from locust import HttpLocust, TaskSet, task, seq_task
 
 class DataMetricsService(TaskSet):
 
     head = {"x-app-key": "804c41d8cfd74926185087402f166277",
-            "uid": "1206540"}
+           "uid": "1206540"}
     vcenterId = '27cf6667-26df-4c72-9d32-424bbca30b09'
     datacenter = 'VxRail-Datacenter'
     clusterId = '522dfb0c-20cc-d2f9-9849-0c18707289b4'
@@ -14,14 +12,14 @@ class DataMetricsService(TaskSet):
     clusterParam = '?cluster_uuid=%s' % clusterId
     clusterParam4usage = '?cluster_uuid=522dfb0c-20cc-d2f9-9849-0c18707289b4'
 
-    # One task per page that calls data-metrics-service
+# One task per page that calls data-metrics-service
 
-    # -----------------------One Datacenter-------------------------------------
+# -----------------------One Datacenter-------------------------------------
     @seq_task(1)
     def logical_dc_performance(self):
         self.hosts_performance_summary(self.dcParam)
 
-    # -----------------------One Cluster---------------------------------------
+# -----------------------One Cluster---------------------------------------
     @seq_task(2)
     def logical_cluster_storage(self):
         self.storage_usage_prediction(self.clusterParam4usage)
@@ -35,27 +33,39 @@ class DataMetricsService(TaskSet):
         self.hosts_performance_details(self.clusterParam)
 
 
-    # APIs in Data-Metrics-Service
+# APIs in Data-Metrics-Service
     def storage_usage_prediction(self, params):
         self.sendGetRequest('/api/v1/storage/usage_prediction' + params)
 
     def hosts_performance_summary(self, params):
-        metricType = 'cpu%2Cmem%2Cdisk%2Cnet'
+        # metricType = 'cpu%2Cmem%2Cdisk%2Cnet'
         # Time range is 24 hours as UI default
-        fromTime = '1555904860148'
-        toTime = '1555912060149'
+        fromTime = '1556935645705'
+        toTime = '1557022045705'
         self.sendGetRequest('/api/v1/hosts/performance/summary%s&metric_type=%s&from_timestamp=%s&to_timestamp=%s'
-                            % (params, metricType, fromTime, toTime))
+                            % (params, 'cpu', fromTime, toTime))
+        self.sendGetRequest('/api/v1/hosts/performance/summary%s&metric_type=%s&from_timestamp=%s&to_timestamp=%s'
+                            % (params, 'mem', fromTime, toTime))
+        self.sendGetRequest('/api/v1/hosts/performance/summary%s&metric_type=%s&from_timestamp=%s&to_timestamp=%s'
+                            % (params, 'disk', fromTime, toTime))
+        self.sendGetRequest('/api/v1/hosts/performance/summary%s&metric_type=%s&from_timestamp=%s&to_timestamp=%s'
+                            % (params, 'net', fromTime, toTime))
 
     def hosts_performance_details(self, params):
-        metricType = 'cpu%2Cmem%2Cdisk%2Cnet'
         # Time range is 24 hours as UI default
-        fromTime = '1555904860148'
-        toTime = '1555912060149'
+        fromTime = '1557014815259'
+        toTime = '1557022015261'
         self.sendGetRequest('/api/v1/hosts/performance/details%s&host_uuid=%s&from_timestamp=%s&to_timestamp=%s&metric_type=%s&%s'
-                            % (params, self.hostId, fromTime, toTime, metricType, 'show_anomalies=true'))
+                            % (params, self.hostId, fromTime, toTime, 'cpu', 'show_anomalies=true'))
+        self.sendGetRequest('/api/v1/hosts/performance/details%s&host_uuid=%s&from_timestamp=%s&to_timestamp=%s&metric_type=%s&%s'
+                            % (params, self.hostId, fromTime, toTime, 'mem', 'show_anomalies=true'))
+        self.sendGetRequest('/api/v1/hosts/performance/details%s&host_uuid=%s&from_timestamp=%s&to_timestamp=%s&metric_type=%s&%s'
+                            % (params, self.hostId, fromTime, toTime, 'disk', 'show_anomalies=true'))
+        self.sendGetRequest('/api/v1/hosts/performance/details%s&host_uuid=%s&from_timestamp=%s&to_timestamp=%s&metric_type=%s&%s'
+                            % (params, self.hostId, fromTime, toTime, 'net', 'show_anomalies=true'))
 
-    # Send Get request
+
+# Send Get request
     def sendGetRequest(self, path):
         self.client.get(path, headers=self.head)
 
@@ -65,4 +75,3 @@ class DashboardUserBehavior(HttpLocust):
     task_set = DataMetricsService
     min_wait = 5000
     max_wait = 9000
-
